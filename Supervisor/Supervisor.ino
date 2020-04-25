789fe2de4488fd60686c617f82e169f10d5432af
abba <abba@nuclearinstruments.eu>
2020-04-25 18:14:49 +0200

    imported the Supervisor code in the repository

---
+++ Supervisor/Supervisor.ino
@@ -0,0 +1,408 @@
+﻿/****************************************************************************
+ * MVM Ventialotor V4 Firmware
+ *
+ * This code is designed to run on the HW V4 of MVM Ventialor
+ *
+ * This code is distributed as DEMO and has NOT been tested and is NOT approved
+ * by quality check of Nuclear Instruments or Elemaster
+ *
+ * !!!!   This code MUST NOT BE USED on final application    !!!!!
+ *
+ * Nuclear Instruments and Elemaster do not take responsibility for damage caused
+ * by using this code and decline any responsability
+ *
+ * This code is property of Nuclear Instruments and distributed
+ * with fair use license
+ *
+ ****************************************************************************/
+
+
+#define SCL_PIN 2
+#define SCL_PORT PORTD
+#define SDA_PIN 3
+#define SDA_PORT PORTD
+#define I2C_TIMEOUT 100
+#define I2C_FASTMODE 0
+
+#include <SoftI2CMaster.h>
+#include <SoftWire.h>
+
+#include <Wire.h>
+
+#define ALARM_PIN_TOO_LOW 0
+#define ALARM_PIN_TOO_HIGH 1
+#define ALARM_TEMP_LOW 2
+#define ALARM_TEMP_HIGH 3
+#define ALARM_ACTIVITY 4
+#define ALARM_POWER 5
+#define ALARM_CHARGE_CRITICAL 6
+
+
+
+
+SoftWire swire = SoftWire();
+
+
+#include "ads1115_soft.h"
+
+#define VALVE_THREE_WAY 8
+#define VALVE_ANALOG A6
+#define BRETHE 6
+#define POWER_STATUS 9
+#define PROBE_12VSYS A1
+#define PROBE_5V_RASP A2
+#define PROBE_BCHARGE A3
+#define NTC_PIN A7
+#define ALARM_RELE 4  
+#define ALARM_RED 7
+#define ALARM_YELLOW 10
+#define ALARM_BUZZER A0
+
+
+bool EnableWdogController = false;
+bool Run = false;
+uint8_t BreetheRate = 15;
+uint8_t dP = 5;
+uint8_t Pin_max = 7;
+uint8_t Pin_min = 3;
+
+int pv2;
+int breethe;
+int pwall;
+int p12v;
+int p5rasp;
+int pv1;
+int pbattery;
+float ntc;
+
+float InPress;
+float Ref25;
+float p33v;
+
+float ReadNTC();
+
+uint32_t wdogtimer = 0;
+
+bool wdog_alarm = false;
+bool power_alarm = false;
+bool in_pressure_alarm = false;
+bool circuit_alarm = false;
+bool activity_alarm = false;
+
+uint16_t ALARM_FLAG = 0;
+
+uint32_t GenerateFlag(int alarm_code)
+{
+    return (1 << alarm_code);
+}
+
+
+void setup() {
+    Serial.begin(115200);
+
+    digitalWrite(ALARM_RELE, 1);
+    digitalWrite(ALARM_RED, 0);
+    digitalWrite(ALARM_YELLOW, 0);
+    digitalWrite(ALARM_BUZZER, 0);
+
+    pinMode(ALARM_RELE, OUTPUT);
+    pinMode(ALARM_RED, OUTPUT);
+    pinMode(ALARM_YELLOW, OUTPUT);
+    pinMode(ALARM_BUZZER, OUTPUT);
+
+    pinMode(VALVE_THREE_WAY, INPUT);
+    pinMode(BRETHE, INPUT);
+    pinMode(POWER_STATUS, INPUT);
+
+
+
+    // put your setup code here, to run once:
+    Wire.begin(0x22);
+    Wire.onReceive(receiveEvent);
+    Wire.onRequest(requestEvent);
+
+
+}
+
+
+void loop() {
+
+    // put your main code here, to run repeatedly:
+
+    bool pv1_bool;
+
+    pv2 = digitalRead(VALVE_THREE_WAY);
+    breethe = digitalRead(BRETHE);
+    pwall = digitalRead(POWER_STATUS);
+    p12v = ((float)analogRead(PROBE_12VSYS) / 1024.0) * 3.3 * 5.0 * 0.93;
+    p5rasp = ((float)analogRead(PROBE_5V_RASP) / 1024.0) * 3.3 * 2.0;
+    pv1 = analogRead(VALVE_ANALOG);
+    pbattery = analogRead(PROBE_BCHARGE) / 10.0;
+    ntc = ReadNTC();
+
+    InPress = (((float)readADC_SingleEnded(0x48, 0)) * 0.0009175277) - 0.2715030471;
+    Ref25 = readADC_SingleEnded(0x48, 2);
+    p33v = ((float)readADC_SingleEnded(0x48, 3)) / Ref25 * 2.5 * 2;
+
+    Serial.println(
+        "Run: " + String(Run) +
+        " pv1: " + String(pv1) +
+        " pv2: " + String(pv2) +
+        " breethe: " + String(breethe) +
+        " pwall: " + String(pwall) +
+        " p12v: " + String(p12v) +
+        " p5rasp: " + String(p5rasp) +
+        " pbattery: " + String(pbattery) +
+        " ntc: " + String(ntc) +
+        " InPress: " + String(InPress) +
+        " Ref25: " + String(Ref25) +
+        " p33v: " + String(p33v) +
+        " ALARM_FLAG: " + String(ALARM_FLAG)
+
+    );
+
+    /*
+      INSERT HERE THE STATE MACHINE THAT CHECK THE VALVE STATUS
+    */
+
+
+    pv1_bool = pv1 > 20 ? true : false;
+
+    /*
+      TRACE OF WHAT TO DO
+
+      STATE MACHINE
+
+      STATE 1 WAIT BREATH SIGNAL GO HIGH
+      STATE 2 WAIT PV1 IS HIGH
+      STATE 3 WAIT BREATH SIGNAL GO LOW
+      STATE 4 WAIT PV2 IS HIGH
+
+      IF THE STATE MACHINE DOES NOT GO THROUGHT ALL THIS STATE
+      TRIGGER ALARM
+
+    */
+
+
+
+
+    /*
+      //Alarms in run only
+      if (Run)
+      {
+        if (InPress < Pin_min)
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_PIN_TOO_LOW);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_PIN_TOO_LOW);
+
+        if (pbattery<5.0)
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_CHARGE_CRITICAL);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_CHARGE_CRITICAL);
+
+      }
+      else
+      {
+
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_PIN_TOO_LOW);
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_CHARGE_CRITICAL);
+      }
+
+        if (InPress > Pin_max)
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_PIN_TOO_HIGH);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_PIN_TOO_HIGH);
+
+        if (ntc < 5)
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_TEMP_LOW);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_TEMP_LOW);
+
+        if (ntc > 80)
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_TEMP_HIGH);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_TEMP_HIGH);
+
+        if ((p12v > 14.5) || (p12v < 10) || (p33v < 3.15) || (p33v > 3.4) || (Ref25 < 19000.00) || (Ref25 > 21000.00))  //aggiungere check raspberry
+          ALARM_FLAG = ALARM_FLAG | GenerateFlag(ALARM_POWER);
+        else
+          ALARM_FLAG = ALARM_FLAG & ~GenerateFlag(ALARM_POWER);
+
+
+    if (ALARM_FLAG!=0)
+    {
+      digitalWrite(ALARM_RELE, 0);
+      digitalWrite(ALARM_RED, 1);
+      digitalWrite(ALARM_BUZZER, 1);
+    }
+    else
+    {
+      digitalWrite(ALARM_RELE, 1);
+      digitalWrite(ALARM_RED, 0);
+      digitalWrite(ALARM_BUZZER, 0);
+
+
+    }
+      //Allarmi sempre attivi
+    */
+
+    delay(25);
+
+
+}
+
+
+
+//Gestione I2C
+unsigned int iic_int_addr;
+unsigned int iic_cast;
+//Ricezione di byte
+void receiveEvent(int howMany) {
+    int x;
+    int status = 0;
+
+    unsigned char databuffer[4];
+    int dba = 0;
+
+    while (0 < Wire.available()) {
+        uint8_t c = Wire.read();
+
+
+        switch (status)
+        {
+        case 0:  iic_int_addr = c;  break;
+        case 1:  databuffer[dba++] = c; break;
+        case 2:  databuffer[dba++] = c;
+
+            if (iic_int_addr == 0x00)
+            {
+                wdogtimer = millis();
+            }
+            if (iic_int_addr == 0x01)
+            {
+                EnableWdogController = databuffer[0];
+            }
+
+            if (iic_int_addr == 0x02)
+            {
+                Run = databuffer[0];
+            }
+
+            if (iic_int_addr == 0x03)
+            {
+                BreetheRate = databuffer[0];
+            }
+
+            if (iic_int_addr == 0x04)
+            {
+                dP = databuffer[0];
+            }
+
+            if (iic_int_addr == 0x05)
+            {
+                Pin_max = databuffer[0];
+            }
+
+            if (iic_int_addr == 0x06)
+            {
+                Pin_min = databuffer[0];
+            }
+
+            break;
+
+
+        }
+
+        status++;
+    }
+
+}
+
+
+
+
+
+void requestEvent() {
+
+    char c;
+    uint16_t value;
+    uint8_t databuffer[4];
+
+    if (iic_int_addr == 0x50)
+    {
+        value = (uint16_t)(InPress > 0 ? InPress : 0) * 1000;
+    }
+
+    if (iic_int_addr == 0x51)
+    {
+        value = (uint16_t)(pbattery > 0 ? pbattery : 0);
+    }
+
+    if (iic_int_addr == 0x52)
+    {
+        value = pwall;
+    }
+
+    if (iic_int_addr == 0x53)
+    {
+        value = p12v * 1000.0;
+    }
+
+    if (iic_int_addr == 0x54)
+    {
+        value = p5rasp * 1000.0;
+    }
+
+    if (iic_int_addr == 0x55)
+    {
+        value = p33v * 1000.0;
+    }
+
+    if (iic_int_addr == 0x56)
+    {
+        value = ntc * 10.0;
+    }
+
+    if (iic_int_addr == 0x57)
+    {
+        value = ALARM_FLAG;
+    }
+
+
+    if (iic_int_addr == 0xFC)
+    {
+        value = 0x2020;
+    }
+
+    if (iic_int_addr == 0xFD)
+    {
+        value = 0x1804;
+    }
+
+    databuffer[0] = value & 0xFF;
+    databuffer[1] = (value >> 8) & 0xFF;
+
+
+    Wire.write(databuffer[0]);
+    Wire.write(databuffer[1]);
+}
+
+
+float ReadNTC() {
+
+    float Rfissa = 10000;  // [ohm]     Resistencia fija del divisor
+    float R25 = 10000;    // [ohm]     Valor de NTC a 25�C
+    float Beta = 3490.0; // [K]      Par�metro Beta de NTC
+    float Beta2 = 3434.0; // [K]      Par�metro Beta de NTC
+    float T0 = 298.15;   // [K]       Temperatura de referencia en Kelvin
+    float Vtmp = 0.0;    // [V]       Variable para almacenar Vout
+    float Rntc = 0.0;    // [ohm]     Variable para NTC en ohmnios
+    float TempK = 0.0;   // [K]       Temperatura salida en Kelvin
+    float TempC = 0.0;   // [�C]      Temperatura salida en Celsius
+
+    Vtmp = (5.0 / 1024) * (analogRead(NTC_PIN));
+    Rntc = (Vtmp * Rfissa) / (5.0 - Vtmp);
+    TempK = Beta / (log(Rntc / R25) + (Beta / T0));
+    TempC = TempK - 273, 15;
+    return TempC;
+}
---
+++ Supervisor/ads1115_soft.h
@@ -0,0 +1,189 @@
+
+// Programmable Gain definitions
+#define MCP47X6_GAIN_MASK  0xFE
+#define MCP47X6_GAIN_1X 0x00
+#define MCP47X6_GAIN_2X 0x01
+
+// Power Down Mode definitions
+#define MCP47X6_PWRDN_MASK     0xF9
+#define MCP47X6_AWAKE          0x00
+#define MCP47X6_PWRDN_1K       0x02
+#define MCP47X6_PWRDN_100K     0x04
+#define MCP47X6_PWRDN_500K     0x06
+
+// Reference Voltage definitions
+#define MCP47X6_VREF_MASK             0xE7
+#define MCP47X6_VREF_VDD              0x00
+#define MCP47X6_VREF_VREFPIN        0x10
+#define MCP47X6_VREF_VREFPIN_BUFFERED 0x18
+
+// Command definitioins
+#define MCP47X6_CMD_MASK       0x1F
+#define MCP47X6_CMD_VOLDAC     0x00
+#define MCP47X6_CMD_VOLALL     0x40
+#define MCP47X6_CMD_VOLCONFIG  0x80
+#define MCP47X6_CMD_ALL        0x60
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define ADS1015_ADDRESS                 (0x48)    // 1001 000 (ADDR = GND)
+/*=========================================================================*/
+
+/*=========================================================================
+    CONVERSION DELAY (in mS)
+    -----------------------------------------------------------------------*/
+    #define ADS1015_CONVERSIONDELAY         (1)
+    #define ADS1115_CONVERSIONDELAY         (8)
+/*=========================================================================*/
+
+/*=========================================================================
+    POINTER REGISTER
+    -----------------------------------------------------------------------*/
+    #define ADS1015_REG_POINTER_MASK        (0x03)
+    #define ADS1015_REG_POINTER_CONVERT     (0x00)
+    #define ADS1015_REG_POINTER_CONFIG      (0x01)
+    #define ADS1015_REG_POINTER_LOWTHRESH   (0x02)
+    #define ADS1015_REG_POINTER_HITHRESH    (0x03)
+/*=========================================================================*/
+
+/*=========================================================================
+    CONFIG REGISTER
+    -----------------------------------------------------------------------*/
+    #define ADS1015_REG_CONFIG_OS_MASK      (0x8000)
+    #define ADS1015_REG_CONFIG_OS_SINGLE    (0x8000)  // Write: Set to start a single-conversion
+    #define ADS1015_REG_CONFIG_OS_BUSY      (0x0000)  // Read: Bit = 0 when conversion is in progress
+    #define ADS1015_REG_CONFIG_OS_NOTBUSY   (0x8000)  // Read: Bit = 1 when device is not performing a conversion
+
+    #define ADS1015_REG_CONFIG_MUX_MASK     (0x7000)
+    #define ADS1015_REG_CONFIG_MUX_DIFF_0_1 (0x0000)  // Differential P = AIN0, N = AIN1 (default)
+    #define ADS1015_REG_CONFIG_MUX_DIFF_0_3 (0x1000)  // Differential P = AIN0, N = AIN3
+    #define ADS1015_REG_CONFIG_MUX_DIFF_1_3 (0x2000)  // Differential P = AIN1, N = AIN3
+    #define ADS1015_REG_CONFIG_MUX_DIFF_2_3 (0x3000)  // Differential P = AIN2, N = AIN3
+    #define ADS1015_REG_CONFIG_MUX_SINGLE_0 (0x4000)  // Single-ended AIN0
+    #define ADS1015_REG_CONFIG_MUX_SINGLE_1 (0x5000)  // Single-ended AIN1
+    #define ADS1015_REG_CONFIG_MUX_SINGLE_2 (0x6000)  // Single-ended AIN2
+    #define ADS1015_REG_CONFIG_MUX_SINGLE_3 (0x7000)  // Single-ended AIN3
+
+    #define ADS1015_REG_CONFIG_PGA_MASK     (0x0E00)
+    #define ADS1015_REG_CONFIG_PGA_6_144V   (0x0000)  // +/-6.144V range = Gain 2/3
+    #define ADS1015_REG_CONFIG_PGA_4_096V   (0x0200)  // +/-4.096V range = Gain 1
+    #define ADS1015_REG_CONFIG_PGA_2_048V   (0x0400)  // +/-2.048V range = Gain 2 (default)
+    #define ADS1015_REG_CONFIG_PGA_1_024V   (0x0600)  // +/-1.024V range = Gain 4
+    #define ADS1015_REG_CONFIG_PGA_0_512V   (0x0800)  // +/-0.512V range = Gain 8
+    #define ADS1015_REG_CONFIG_PGA_0_256V   (0x0A00)  // +/-0.256V range = Gain 16
+
+    #define ADS1015_REG_CONFIG_MODE_MASK    (0x0100)
+    #define ADS1015_REG_CONFIG_MODE_CONTIN  (0x0000)  // Continuous conversion mode
+    #define ADS1015_REG_CONFIG_MODE_SINGLE  (0x0100)  // Power-down single-shot mode (default)
+
+    #define ADS1015_REG_CONFIG_DR_MASK      (0x00E0)  
+    #define ADS1015_REG_CONFIG_DR_128SPS    (0x0000)  // 128 samples per second
+    #define ADS1015_REG_CONFIG_DR_250SPS    (0x0020)  // 250 samples per second
+    #define ADS1015_REG_CONFIG_DR_490SPS    (0x0040)  // 490 samples per second
+    #define ADS1015_REG_CONFIG_DR_920SPS    (0x0060)  // 920 samples per second
+    #define ADS1015_REG_CONFIG_DR_1600SPS   (0x0080)  // 1600 samples per second (default)
+    #define ADS1015_REG_CONFIG_DR_2400SPS   (0x00A0)  // 2400 samples per second
+    #define ADS1015_REG_CONFIG_DR_3300SPS   (0x00C0)  // 3300 samples per second
+
+    #define ADS1015_REG_CONFIG_CMODE_MASK   (0x0010)
+    #define ADS1015_REG_CONFIG_CMODE_TRAD   (0x0000)  // Traditional comparator with hysteresis (default)
+    #define ADS1015_REG_CONFIG_CMODE_WINDOW (0x0010)  // Window comparator
+
+    #define ADS1015_REG_CONFIG_CPOL_MASK    (0x0008)
+    #define ADS1015_REG_CONFIG_CPOL_ACTVLOW (0x0000)  // ALERT/RDY pin is low when active (default)
+    #define ADS1015_REG_CONFIG_CPOL_ACTVHI  (0x0008)  // ALERT/RDY pin is high when active
+
+    #define ADS1015_REG_CONFIG_CLAT_MASK    (0x0004)  // Determines if ALERT/RDY pin latches once asserted
+    #define ADS1015_REG_CONFIG_CLAT_NONLAT  (0x0000)  // Non-latching comparator (default)
+    #define ADS1015_REG_CONFIG_CLAT_LATCH   (0x0004)  // Latching comparator
+
+    #define ADS1015_REG_CONFIG_CQUE_MASK    (0x0003)
+    #define ADS1015_REG_CONFIG_CQUE_1CONV   (0x0000)  // Assert ALERT/RDY after one conversions
+    #define ADS1015_REG_CONFIG_CQUE_2CONV   (0x0001)  // Assert ALERT/RDY after two conversions
+    #define ADS1015_REG_CONFIG_CQUE_4CONV   (0x0002)  // Assert ALERT/RDY after four conversions
+    #define ADS1015_REG_CONFIG_CQUE_NONE    (0x0003)  // Disable the comparator and put ALERT/RDY in high state (default)
+/*=========================================================================*/
+
+typedef enum
+{
+  GAIN_TWOTHIRDS    = ADS1015_REG_CONFIG_PGA_6_144V,
+  GAIN_ONE          = ADS1015_REG_CONFIG_PGA_4_096V,
+  GAIN_TWO          = ADS1015_REG_CONFIG_PGA_2_048V,
+  GAIN_FOUR         = ADS1015_REG_CONFIG_PGA_1_024V,
+  GAIN_EIGHT        = ADS1015_REG_CONFIG_PGA_0_512V,
+  GAIN_SIXTEEN      = ADS1015_REG_CONFIG_PGA_0_256V
+} adsGain_t;
+
+
+
+
+static void writeRegister(uint8_t i2cAddress, uint8_t reg, uint16_t value) {
+  i2c_start(((uint8_t)i2cAddress<<1) | I2C_WRITE);
+  i2c_write((uint8_t)reg);
+  i2c_write((uint8_t)(value>>8));
+  i2c_write((uint8_t)(value & 0xFF));
+  i2c_stop();
+}
+
+static uint16_t readRegister(uint8_t i2cAddress, uint8_t reg) {
+  uint16_t dato;
+  i2c_start(((uint8_t)i2cAddress<<1) | I2C_WRITE);
+  i2c_write(ADS1015_REG_POINTER_CONVERT);
+  i2c_stop();
+  i2c_start(((uint8_t)i2cAddress<<1) | I2C_READ);
+  dato = (i2c_read(false) << 8);
+  dato += (i2c_read(true));
+  i2c_stop();
+
+  return dato;
+}
+
+uint16_t readADC_SingleEnded(uint8_t i2cAddress, uint8_t channel) {
+  if (channel > 3)
+  {
+    return 0;
+  }
+  int m_gain=GAIN_ONE;
+  
+  // Start with default values
+  uint16_t config = ADS1015_REG_CONFIG_CQUE_NONE    | // Disable the comparator (default val)
+                    ADS1015_REG_CONFIG_CLAT_NONLAT  | // Non-latching (default val)
+                    ADS1015_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+                    ADS1015_REG_CONFIG_CMODE_TRAD   | // Traditional comparator (default val)
+                    ADS1015_REG_CONFIG_DR_1600SPS   | // 1600 samples per second (default)
+                    ADS1015_REG_CONFIG_MODE_SINGLE;   // Single-shot mode (default)
+
+  // Set PGA/voltage range
+  config |= m_gain;
+
+  // Set single-ended input channel
+  switch (channel)
+  {
+    case (0):
+      config |= ADS1015_REG_CONFIG_MUX_SINGLE_0;
+      break;
+    case (1):
+      config |= ADS1015_REG_CONFIG_MUX_SINGLE_1;
+      break;
+    case (2):
+      config |= ADS1015_REG_CONFIG_MUX_SINGLE_2;
+      break;
+    case (3):
+      config |= ADS1015_REG_CONFIG_MUX_SINGLE_3;
+      break;
+  }
+
+  // Set 'start single-conversion' bit
+  config |= ADS1015_REG_CONFIG_OS_SINGLE;
+
+  // Write config register to the ADC
+  writeRegister(i2cAddress, ADS1015_REG_POINTER_CONFIG, config);
+
+  // Wait for the conversion to complete
+  delay(ADS1115_CONVERSIONDELAY);
+  
+  // Read the conversion results
+  // Shift 12-bit results right 4 bits for the ADS1015
+  return readRegister(i2cAddress, ADS1015_REG_POINTER_CONVERT);  
+}
